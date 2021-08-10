146e6b009f4a0c88080516e7a45b52048b2f4c1e
Luis <luisdiaslima25@gmail.com>
2021-08-10 19:54:34 -0300

    create login

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,33 @@
 </template>
 
 <script>
+import { watch } from '@vue/runtime-core'
+import { useRoute, useRouter } from 'vue-router'
+import { setCurrentUser } from './store/user'
+
 import ModalFactory from './components/ModalFactory'
+import services from './services'
 
 export default {
-  components: { ModalFactory }
+  components: { ModalFactory },
+  setup () {
+    const router = useRouter()
+    const route = useRoute()
+
+    watch(() => route.path, async () => {
+      if (route.meta.hasAuth) {
+        const token = localStorage.getItem('token')
+
+        if (!token) {
+          router.push({ name: 'Home' })
+          return
+        }
+
+        const { data } = await services.users.getMe()
+        setCurrentUser(data)
+        console.log(data)
+      }
+    })
+  }
 }
 </script>
--- src/views/Feedbacks/index.vue
+++ src/views/Feedbacks/index.vue
@@ -1,13 +1,23 @@
 <template>
-  <h1>feedbacks</h1>
+  <h1> Bem-Vindo {{ state.user.name }} </h1>
 </template>
 
 <script>
+import { reactive } from '@vue/reactivity'
+import useStore from '../../hooks/useStore'
+const user = useStore('User')
 export default {
+  setup () {
+    const state = reactive({
+      user: user.currentUser
+    })
 
+    return {
+      state
+    }
+  }
 }
 </script>
-
 <style>
 
 </style>
